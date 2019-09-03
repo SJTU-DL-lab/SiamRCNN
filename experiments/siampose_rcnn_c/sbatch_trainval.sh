faef1505fcea7e3c4c13b115a463e3d224dd3a0a
ShunyuYao <ysy2017@sjtu.edu.cn>
2019-09-03 10:33:57 +0800

    t

--- experiments/siampose_rcnn_c/sbatch_trainval.sh
+++ experiments/siampose_rcnn_c/sbatch_trainval.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH -J siam_rcnn_featEn_0819
+#SBATCH -J siam_rcnn_test
 #SBATCH -p gpu
-#SBATCH --output=siamrcnn_bk_0819.out
-#SBATCH --error=siamrcnn_bk_0819.err
-#SBATCH --gres=gpu:6
-#SBATCH --nodelist=node6
+#SBATCH --output=siamrcnn_test.out
+#SBATCH --error=siamrcnn_test.err
+#SBATCH --gres=gpu:1
+#SBATCH --nodelist=node5
 date
 module load anaconda2/5.3.0 cuda/9.0 cudnn/7.3.0
-source activate deepimit
+source activate pytorch0.4
 
 # ROOT=/cluster/home/it_stu2/SiamPose
 # ROOT=`git rev-parse --show-toplevel`
@@ -19,13 +19,13 @@ export PYTHONPATH=$ROOT:$PYTHONPATH
 mkdir -p logs
 
 python -u $ROOT/tools/trainval_siamrcnn.py \
-    --config=config.json -b 96 \
+    --config=config.json -b 1 \
     -j 4 --save_freq 2 \
     --epochs 200 --hm_hp_weight 1.5 \
-    --log-dir board/show_0819 \
-    --save_dir snapshot_0819 \
-    --log logs/log_0819.txt \
-    --resume snapshot_0819/checkpoint_e63.pth
+    --log-dir board/show_test \
+    --save_dir snapshot_test \
+    --log logs/log_test.txt
+    --pretrained ../siampose_rcnn_base/snapshot_0902/checkpoint_e19.pth
     # --pretrained snapshot_0815/checkpoint_e199.pth
     # --pretrained snapshot_full_img/checkpoint_e88.pth
     2>&1 | tee logs/train.log
